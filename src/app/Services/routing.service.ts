dac4b74ccd3e9d60d9d88b63a5e6407e8de14749
AlbertBabaiani <BabaianiAlbert@gmail.com>
2024-03-25 00:12:40 +0400

    Fixed routing

--- src/app/Services/routing.service.ts
+++ src/app/Services/routing.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, OnDestroy, inject } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute, ParamMap, Router, NavigationEnd } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 import { Filters } from '../Models/Filter_types';
+
+type FilterQueryResponse = Filters | null | false
 
 @Injectable({
   providedIn: 'root'
@@ -9,62 +11,89 @@ import { Filters } from '../Models/Filter_types';
 export class RoutingService implements OnDestroy{
 
   private router: Router = inject(Router)
+  private router_navigation_subscription!: Subscription
 
   private activated_route: ActivatedRoute = inject(ActivatedRoute)
   private activated_route_subscription!: Subscription
 
+  
+
   constructor() {
+    this.router_navigation_subscription = this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+
+      let url = event.url.slice(0,5)
+
+      if (url === '/List' || url === '/') {       
+        this.observing_queries()
+      }
+    });
+  }
+
+  private observing_queries(): void{
     this.activated_route_subscription = this.activated_route.queryParamMap.subscribe({
       next: (query: ParamMap) => {
-        const route = this.router.url
 
-        if(route.slice(0, 6) !== '/Error'){
-          const response_names = this.check_all_queries(query)
+        const query_names_check = this.check_all_queries(query)
+
+
+        if(query_names_check){
+ 
+          const query_filter_check: FilterQueryResponse = this.check_filter_query(query)
           
-          if(response_names){
-            this.default_filter_type(query)
+          if(query_filter_check === 'All' || query_filter_check === 'Completed' || query_filter_check === 'Uncompleted'){
+
+            this.router.navigate(['./'], {
+              relativeTo: this.activated_route,
+              queryParams: { Filter: query_filter_check },
+              queryParamsHandling: 'merge'
+            });
           }
+
+          else if(query_filter_check === null){
+            this.router.navigate(['./'], {
+              relativeTo: this.activated_route,
+              queryParams: { Filter: 'All' },
+              queryParamsHandling: 'merge'
+            });
+          }
+
           else{
-            this.router.navigate(['Error'])
+  
+            this.router.navigate(['Error']);
           }
+
+        }
+        else{
+          this.router.navigate(['Error']);
         }
       }
     })
   }
 
   private check_all_queries(queryParams: ParamMap): boolean{
+    console.log(queryParams)
     return queryParams.keys.every((query: string) => {
-      return query === 'Sort' || query === 'Filter' || query === '' || query === null || query === undefined;
+      return query === 'Filter' || query === '' || query === null || query === undefined;
     });
   }
 
-  private check_filter_query(raw_query: ParamMap): boolean{
+  private check_filter_query(raw_query: ParamMap): FilterQueryResponse {
     const query: string | null | undefined = raw_query.get('Filter')
 
-    if(query === 'All' || query === 'Completed' || query === 'Uncompleted' || query === undefined || query === null){
-      return true
+    if(query === 'All' || query === 'Completed' || query === 'Uncompleted'){
+      return query
     }
-    else return false
-  }
-
-  private default_filter_type(query: ParamMap): void{   
-
-    const response = this.check_filter_query(query)
-
-    if(response){
-      const filter_temp = query.get('Filter')
-      
-      if(filter_temp === '' || filter_temp === undefined || filter_temp === null){
-        this.router.navigate(['List'], {relativeTo: this.activated_route, queryParams: { Filter: 'All' }, queryParamsHandling: 'merge'})
-      }
-      else{
-        this.router.navigate(['List'], {relativeTo: this.activated_route, queryParams: { Filter: filter_temp }, queryParamsHandling: 'merge'})
-      }
+    else if(query === null || query === undefined){
+      return null
     }
     else{
-      this.router.navigate(['Error'])
+      console.log('NOOOOOO')
+      return false
     }
   }
+
 
   change_filter_type(filter: Filters): void{
     this.router.navigate(['List'], {relativeTo: this.activated_route, queryParams: { Filter: filter }, queryParamsHandling: 'merge'})
@@ -73,6 +102,10 @@ export class RoutingService implements OnDestroy{
   ngOnDestroy(): void {
     if(this.activated_route_subscription){
       this.activated_route_subscription.unsubscribe()
+    }
+
+    if(this.router_navigation_subscription){
+      this.router_navigation_subscription.unsubscribe()
     }
   }
 }
